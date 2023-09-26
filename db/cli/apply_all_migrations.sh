5ff391aebbdcd11d85eb18cb3ff4b25c4a4fa553
Adrian <adrianprelipceanc@gmail.com>
2023-09-26 16:27:43 +0200

    ci/cd fixes

--- db/cli/apply_all_migrations.sh
+++ db/cli/apply_all_migrations.sh
@@ -1,4 +1,5 @@
-for f in source/migrations*.sql;
+echo 'Applying migrations'
+for f in source/migrations/*.sql;
 do
     if test -f "$f"; then
     psql -U $USER -h $HOST -p $PORT -d $DATABASE -v "ON_ERROR_STOP=1" -f "$f"
