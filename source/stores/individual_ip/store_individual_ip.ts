3106bca445935458df497f1ae30d18e3f4534879
Adrian <adrianprelipceanc@gmail.com>
2023-09-26 12:44:55 +0200

    fix: decrement calls correct query

--- source/stores/individual_ip/store_individual_ip.ts
+++ source/stores/individual_ip/store_individual_ip.ts
@@ -134,7 +134,7 @@ class PostgresStoreIndividualIP implements Store {
             WITH 
             rows_to_delete AS (
                 SELECT id FROM rate_limit.individual_records
-                WHERE key = $1 and session_id = $2 ORDER BY registered_at LIMIT 1
+                WHERE key = $1 and session_id = $2 ORDER BY event_time LIMIT 1
                 )
             DELETE FROM rate_limit.individual_records 
               USING rows_to_delete WHERE individual_records.id = rows_to_delete.id            
