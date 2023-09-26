24762116c02d69e00d7ffb909cea6e26d04bffc1
Adrian <adrianprelipceanc@gmail.com>
2023-09-26 15:36:56 +0200

    meta: updated linting to include database folder

--- db/linting/lint.sh
+++ db/linting/lint.sh
@@ -1,2 +1,5 @@
 echo 'Linting migrations folder'
 sqlfluff lint source/migrations/ --dialect postgres --ignore parsing --config db/linting/.sqlfluff 
+
+echo 'Linting tests folder'
+sqlfluff lint db/tests/performance/ --dialect postgres --ignore parsing --config db/linting/.sqlfluff 
