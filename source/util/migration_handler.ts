9c584de5b1828b042a845a994e76b297b37ad088
Alex Fu <alexfu@fastmail.com>
2024-03-17 17:25:58 -0400

    switch to pg.Client

--- source/util/migration_handler.ts
+++ source/util/migration_handler.ts
@@ -1,7 +1,12 @@
-import { Pool } from 'pg'
+import { Client } from 'pg'
 import { migrate } from 'postgres-migrations'
 
 export async function applyMigrations(config: any): Promise<void> {
-	const pool = new Pool(config)
-	await migrate({ client: pool }, __dirname + '/migrations')
+	const client = new Client(config)
+	await client.connect()
+	try {
+		await migrate({ client }, __dirname + '/migrations')
+	} finally {
+		await client.end()
+	}
 }
